211d2b9598e12daf6d147a83e10624d0d905f58a
thirdcrub <chaiwat_satirwattana@cmu.ac.th>
2020-03-24 17:47:19 +0700

    Updates

---
+++ precache-manifest.f199585fc542e8f77a5c8e9691d39b3b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4d6a361592b6e25332e43395d8afbee",
+    "url": "/Lab-08/index.html"
+  },
+  {
+    "revision": "1f2821c00f69329c579f",
+    "url": "/Lab-08/static/css/main.4851e9fc.chunk.css"
+  },
+  {
+    "revision": "ffb844c3f8be98d7db98",
+    "url": "/Lab-08/static/js/2.8f45cdbf.chunk.js"
+  },
+  {
+    "revision": "9cc59aa88bbb965336535eb6b360773d",
+    "url": "/Lab-08/static/js/2.8f45cdbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f2821c00f69329c579f",
+    "url": "/Lab-08/static/js/main.51910ecb.chunk.js"
+  },
+  {
+    "revision": "38d935fcaf6e0490a36f",
+    "url": "/Lab-08/static/js/runtime-main.16490d8d.js"
+  }
+]);
